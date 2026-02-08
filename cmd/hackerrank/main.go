3c483843b89bb7370bba958f6eed5d4eead89e97
yossrez <yosssdev@gmail.com>
2026-02-08 23:40:14 +0700

    hackerrank: difficulty

--- cmd/hackerrank/main.go
+++ cmd/hackerrank/main.go
@@ -6,7 +6,7 @@ import (
 
 	_ "github.com/yossrez/prep-go/internal/hackerrank/certify/basics/modstrings"
 	_ "github.com/yossrez/prep-go/internal/hackerrank/certify/basics/parity"
-	_ "github.com/yossrez/prep-go/internal/hackerrank/fizzbuzz"
+	_ "github.com/yossrez/prep-go/internal/hackerrank/easy/fizzbuzz"
 	"github.com/yossrez/prep-go/internal/hackerrank/runner"
 )
 
